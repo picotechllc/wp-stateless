3f0766c6f712c703808ecdc7c1bbd59cc90a5ecd
Charley Sheets <rcsheets@picote.ch>
2022-08-28 23:32:51 -0700

    try to write an actual unit test

--- tests/test-class-gs-client.php
+++ tests/test-class-gs-client.php
@@ -8,9 +8,19 @@
 use wpCloud\StatelessMedia\GS_Client;
 
 class GS_ClientTest extends WP_UnitTestCase {
+    public function setUp() {
+        parent::setUp();
+    }
+    public function tearDown() {
+        parent::tearDown();
+    }
 	public function test_get_instance() {
-        // This should probably call get_instance() but it doesn't yet. At this point I just hope it runs.
-		// Replace this with some actual testing code.
-		$this->assertTrue( true );
+        $args = array(
+            client_id => "foo",
+            service_account_name => "bar",
+            key_file_path => "baz",
+        );
+        $instance = GS_Client::get_instance($args);
+		$this->assert( gettype($instance) == 'object', 'get_instance should return an object' );
 	}
 }
