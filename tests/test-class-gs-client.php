e572346766e3edd8dc70af542881e7c5b01be984
Charley Sheets <rcsheets@picote.ch>
2022-08-29 01:27:29 -0700

    add another assertion to the test

--- tests/test-class-gs-client.php
+++ tests/test-class-gs-client.php
@@ -15,12 +15,15 @@ class GS_ClientTest extends WP_UnitTestCase {
         parent::tearDown();
     }
 	public function test_get_instance() {
-        $args = array(
+        $args_ok = array(
             'client_id' => "foo",
             'service_account_name' => "bar",
             'key_file_path' => "baz",
         );
-        $instance = GS_Client::get_instance($args);
+        $instance = GS_Client::get_instance($args_ok);
 		$this->assertIsObject($instance, 'get_instance should return an object' );
+        $args_empty = array();
+        $this->expectExceptionMessage('<b>Bucket</b> parameter must be provided.');
+        GS_Client::get_instance($args_empty);
 	}
 }
