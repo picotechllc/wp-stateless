834be1156ab0931e841ed114a0bad5267d66a157
Charley Sheets <rcsheets@picote.ch>
2022-08-28 23:45:56 -0700

    fix array literal that i forgot how to write

--- tests/test-class-gs-client.php
+++ tests/test-class-gs-client.php
@@ -16,9 +16,9 @@ class GS_ClientTest extends WP_UnitTestCase {
     }
 	public function test_get_instance() {
         $args = array(
-            client_id => "foo",
-            service_account_name => "bar",
-            key_file_path => "baz",
+            'client_id' => "foo",
+            'service_account_name' => "bar",
+            'key_file_path' => "baz",
         );
         $instance = GS_Client::get_instance($args);
 		$this->assert( gettype($instance) == 'object', 'get_instance should return an object' );
