1f9497aafbddc476a9ad3348f06731f11a4bfe32
Charley Sheets <rcsheets@picote.ch>
2022-08-28 23:57:17 -0700

    use assertInternalType (which might exist)

--- tests/test-class-gs-client.php
+++ tests/test-class-gs-client.php
@@ -21,6 +21,6 @@ class GS_ClientTest extends WP_UnitTestCase {
             'key_file_path' => "baz",
         );
         $instance = GS_Client::get_instance($args);
-		$this->assert( gettype($instance) == 'object', 'get_instance should return an object' );
+		$this->assertInternalType('object', gettype($instance), 'get_instance should return an object' );
 	}
 }
