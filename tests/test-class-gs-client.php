ac5a89b9f4531c2958ddba2cfa7f9598d9ec2154
Charley Sheets <rcsheets@picote.ch>
2022-10-19 21:08:33 -0700

    refer to some newer phpunit docs and see what happens

--- tests/test-class-gs-client.php
+++ tests/test-class-gs-client.php
@@ -7,23 +7,18 @@
 
 use wpCloud\StatelessMedia\GS_Client;
 
-class GS_ClientTest extends WP_UnitTestCase {
-    public function setUp(): void {
-        parent::setUp();
-    }
-    public function tearDown(): void {
-        parent::tearDown();
-    }
-	public function test_get_instance() {
+final class GS_ClientTest extends PHPUnit\Framework\TestCase
+{
+    public function test_get_instance() {
         $args_ok = array(
             'client_id' => "foo",
             'service_account_name' => "bar",
             'key_file_path' => "baz",
         );
         $instance = GS_Client::get_instance($args_ok);
-		$this->assertIsObject($instance, 'get_instance should return an object' );
+        $this->assertIsObject($instance, 'get_instance should return an object' );
         $args_empty = array();
-        $this->expectException(Exception);
+        $this->expectException('Exception');
         GS_Client::get_instance($args_empty);
-	}
+    }
 }
