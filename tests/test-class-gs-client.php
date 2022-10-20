6c4b3bc2b5eaab54e12e182708f0f100c52724fe
Charley Sheets <rcsheets@picote.ch>
2022-10-19 22:43:28 -0700

    add client_secret key to key_json

--- tests/test-class-gs-client.php
+++ tests/test-class-gs-client.php
@@ -13,8 +13,7 @@ final class GS_ClientTest extends PHPUnit\Framework\TestCase
         // this is intended to be a (barely, technically) valid argument array
         $args_ok = array(
             'bucket' => "pail",
-            'key_json' => '{"private_key":0,"client_id":1}',
-            'client_id' => "foo-client",
+            'key_json' => '{"private_key":0,"client_id":1,"client_secret":2}',
         );
         $instance = GS_Client::get_instance($args_ok);
         $this->assertIsObject($instance, 'get_instance should return an object');
