ab5aca8b91baada81275d35a5fa1fc8811b27155
Charley Sheets <rcsheets@picote.ch>
2022-10-19 22:14:39 -0700

    add another missing required value and some comments

--- tests/test-class-gs-client.php
+++ tests/test-class-gs-client.php
@@ -10,8 +10,10 @@ use wpCloud\StatelessMedia\GS_Client;
 final class GS_ClientTest extends PHPUnit\Framework\TestCase
 {
     public function test_get_instance() {
+        // this is intended to be a (barely, technically) valid argument array
         $args_ok = array(
             'bucket' => "pail",
+            'key_json' => "{\"private_key\":0}",
             'client_id' => "foo",
             'service_account_name' => "bar",
             'key_file_path' => "baz",
@@ -19,6 +21,8 @@ final class GS_ClientTest extends PHPUnit\Framework\TestCase
         $instance = GS_Client::get_instance($args_ok);
         $this->assertIsObject($instance, 'get_instance should return an object' );
         $this->assertNotInstanceOf('WP_Error', $instance, 'get_instance should not return an error, but returned these: ' . implode($instance->get_error_messages()));
+
+        // this empty array is not a valid argument to get_instance and should result in an error being returned
         $args_empty = array();
         $should_be_error = GS_Client::get_instance($args_empty);
         $this->assertInstanceOf('WP_Error', $should_be_error);
