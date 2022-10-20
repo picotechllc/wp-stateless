a051a77aac2b99193309d1d161866f2cb356f3e8
Charley Sheets <rcsheets@picote.ch>
2022-10-19 22:53:40 -0700

    maybe this will work if the success case is last

--- tests/test-class-gs-client.php
+++ tests/test-class-gs-client.php
@@ -10,6 +10,11 @@ use wpCloud\StatelessMedia\GS_Client;
 final class GS_ClientTest extends PHPUnit\Framework\TestCase
 {
     public function test_get_instance() {
+        // this empty array is not a valid argument to get_instance and should result in an error being returned
+        $args_empty = array();
+        $should_be_error = GS_Client::get_instance($args_empty);
+        $this->assertInstanceOf('WP_Error', $should_be_error);
+
         // this is intended to be a (barely, technically) valid argument array
         $args_ok = array(
             'bucket' => "pail",
@@ -18,10 +23,5 @@ final class GS_ClientTest extends PHPUnit\Framework\TestCase
         $instance = GS_Client::get_instance($args_ok);
         $this->assertIsObject($instance, 'get_instance should return an object');
         $this->assertNotInstanceOf('WP_Error', $instance, 'get_instance should not return an error');
-
-        // this empty array is not a valid argument to get_instance and should result in an error being returned
-        $args_empty = array();
-        $should_be_error = GS_Client::get_instance($args_empty);
-        $this->assertInstanceOf('WP_Error', $should_be_error);
     }
 }
